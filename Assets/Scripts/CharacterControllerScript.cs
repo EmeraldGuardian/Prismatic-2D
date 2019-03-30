ee5033ae08163bdd343e31cb91abb2a47a7505eb
Bryce <bryce.j.reid@gmail.com>
2019-03-30 16:17:38 -0400

    original character is working. blue level has been added.

---
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerScript : MonoBehaviour
+{
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+    Rigidbody rigid2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid2D = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        //float move = Input.GetKeyDown("D");
+
+        rigid2D.velocity = new Vector2(move * maxSpeed, rigid2D.velocity.y);
+
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
