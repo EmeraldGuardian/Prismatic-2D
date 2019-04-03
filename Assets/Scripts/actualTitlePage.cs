2c400d4a3d2639dd13831681de92886d6a1dcadc
Bryce <bryce.j.reid@gmail.com>
2019-04-02 22:50:10 -0400

    completed and working Prismatic Demo

---
+++ Assets/Scripts/actualTitlePage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class actualTitlePage : MonoBehaviour
+{
+    public string SceneSelection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void loadIntro()
+    {
+        SceneManager.LoadScene(SceneSelection);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            loadIntro();
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/loadingScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadingScript : MonoBehaviour
+{
+    public string scene;
+    public int time;
+    void Start()
+    {
+        StartCoroutine(loadScene());
+    }
+
+    IEnumerator loadScene()
+    {
+  
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(scene);
+
+    }
+}
